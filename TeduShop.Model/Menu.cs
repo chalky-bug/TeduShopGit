1f9720dca4098c3d8d5768b6e8de9e48f3df4ee1
chalkybug <nguyen1002hn@gmail.com>
2018-03-11 22:32:56 +0700

    update

---
+++ TeduShop.Model/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model
+{
+    [Table("Menus")]
+    public class Menu
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+
+        public int? DisplayOrder { get; set; }
+
+        [Required]
+        public int GroupID { get; set; }
+
+        [Required]
+        public string Target { get; set; }
+        [Required]
+        public bool Status { get; set; }
+    }
+}
---
+++ TeduShop.Model/MenuGroup.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model
+{
+    [Table("MenuGroups")]
+    public class MenuGroup 
+    {
+        [Key]
+        public string ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
